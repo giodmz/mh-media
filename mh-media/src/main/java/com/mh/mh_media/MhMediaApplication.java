b405c1b71fd423a584481bbafd5a62f214ab0dc1
Giovane Damazio <giovxnedamazio@gmail.com>
2026-04-29 16:50:50 -0300

    initial commit

---
+++ mh-media/src/main/java/com/mh/mh_media/MhMediaApplication.java
@@ -0,0 +1,13 @@
+package com.mh.mh_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhMediaApplication.class, args);
+	}
+
+}
---
+++ mh-media/src/test/java/com/mh/mh_media/MhMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.mh_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
